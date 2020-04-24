5a545fd81bbfedefe5dd453374e67680a77d0afc
karanpatel3 <39688706+karanpatel3@users.noreply.github.com>
2020-04-24 19:38:18 -0400

    Update GitHubScraper.py

--- GitHubScraper.py
+++ GitHubScraper.py
@@ -1,9 +1,7 @@
 from github import Github
 import json
-# import matplotlib.pyplot as plt
-"""
-This is the test file of the BackendTest
-"""
+
+
 class GetRepo:
     def __init__(self, acc_tok):
         self.g = Github("{}". format(acc_tok))
@@ -23,16 +21,3 @@ class GetRepo:
                 
                 value = data[key] #value being set to get the value of key
                 self.emp_dict[key] = self.emp_dict[key] + value  
-obj = GetRepo()
-obj.repo_getter() # Update empty Dictionary
-obj.get_keyval() # Sum up all the respective dictionary key values
-print(obj.emp_dict) # Print final dictionary
-
-# plt.title('Bytes of Code in all Github Repositiries')
-# plt.xlabel('Bytes ------->')
-# plt.ylabel('Languages --------->')
-# plt.tick_params(axis='x', colors='Blue')
-# plt.tick_params(axis='y', colors='Red')
-# plt.bar(range(len(obj.emp_dict)), list(obj.emp_dict.values()), align='center', color = ['Red', 'Yellow'])
-# plt.xticks(range(len(obj.emp_dict)), list(obj.emp_dict.keys()))
-# plt.show()

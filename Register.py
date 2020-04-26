03573a42283b5106e5c17d388ffc70e2730708f3
karanpatel3 <39688706+karanpatel3@users.noreply.github.com>
2020-04-26 02:40:46 -0400

    Update Register.py

--- Register.py
+++ Register.py
@@ -1,19 +1,17 @@
 from flask import Flask, jsonify, request, json
-import psycopg2
-import hashlib
-import logging
-
-logging.info("my app has started")
+import psycopg2, hashlib
+from CallScraper import GetLang
 
 app = Flask(__name__)
 @app.route('/')
 #Variables below to put into query, convert these to be POSTED later
 @app.route('/register')
-def Register(f, l, a, e, p):
-
+def Register(f, l, a, g, e, p):
+    
     fname = f
     lname = l
     actoken = a
+    gituser = g
     email = e
     password = hashlib.sha256(p.encode())
     hashedpass= password.hexdigest()
@@ -24,12 +22,14 @@ def Register(f, l, a, e, p):
     sql += "  first_name"
     sql += ", last_name"
     sql += ",  access_token"
+    sql += ", github_name"
     sql += ", email"
     sql += ", pass"
     sql += ") VALUES ("
     sql += " '" + fname + "'"
     sql += ",'" + lname + "'"
     sql += ",'" + actoken + "'"
+    sql += ",'" + gituser + "'"
     sql += ",'" + email + "'"
     sql += ",'" + hashedpass + "'"
     sql += ")"
@@ -43,13 +43,7 @@ def Register(f, l, a, e, p):
    
     try:
         cur.execute(sql)
-##        cur.execute("SELECT * from acct_logins")
 
-
-##    #Uncomment this block to see what's in the acct_logins
-##        db=cur.fetchall()
-##        print(db)
-##        connection.commit()
 
     except psycopg2.Error as e:
         message = "Database error: " + e + "/n SQL: " + sql
@@ -61,16 +55,18 @@ def Register(f, l, a, e, p):
     result = 'true'
     message = "Your user account has been added."
     print(message)
+    GetLang(gituser, actoken)
     cur.close()
     return result
    
 
 if __name__ =="__main__":
-    app.debug = True
-    app.run(host = '0.0.0.0', port = 5000)
-    f = 'Ashish'
+    # app.debug = True
+    # app.run(host = '0.0.0.0', port = 5000)
+    f = 'Ashis'
     l = 'Gare'
-    a = 'WORKING'
+    a = '944a69adfc88a90271f1c5c3b47dc1d577db4c58'
+    g = 'ashish'
     e = 'avg53@rutgers.edu'
     p = 'yes'
-    print(Register(f, l, a, e, p))
+    print(Register(f, l, a, g, e, p))

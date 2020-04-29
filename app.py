ed262729ebcb6e2a90c514daf182c25b1490d5a6
Karan Patel <karan.patel1@rutgers.edu>
2020-04-29 02:10:36 -0400

    new

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_cors import CORS, cross_origin
+# from flask_cors import CORS, cross_origin
 from Login import Login
 from Register import Register
 from updatetoken import Update
